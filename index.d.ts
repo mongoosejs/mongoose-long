f27b63957fae3a5257e1d38e64c0851d468ac359
Valeri Karpov <val@karpov.io>
2021-08-10 18:18:06 -0400

    fix(index.d.ts): add return type to default export
    Fix #14
    Fix Automattic/mongoose#10470

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import { Mongoose } from "mongoose";
 import { SchemaType } from "mongoose";
 import mongooseLong from ".";
 
-export default function(mongoose?: Mongoose);
+export default function(mongoose?: Mongoose): SchemaType;
 
 declare module 'mongoose' {
   namespace Schema {
